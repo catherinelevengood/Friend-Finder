0bbc4eb91b8dfa34ae9b25d06f8e460f6e4b51a3
Catherine Levengood <angel_avelanes@yahoo.com>
2018-10-29 19:51:55 -0400

    install json,set up server

--- server.js
+++ server.js
@@ -1,30 +1,25 @@
-var  express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var path = require('path');
+// Dependencies
+var express = require("express");
 
-// Sets an initial port,We'll use this later in our listener
+
+var api = require('./app/routing/apiRoutes');
+var html = require('./app/routing/htmlRoutes');
+
+// Sets up the Express App
+
+var app = express();
 var PORT = process.env.PORT || 3000;
 
-
- 
-//Set up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: false}));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Parse and HTML body into a string.
-app.use(bodyParser.text({type: 'text/html'}));
 
-//Router
-// Points server to series of route files.
-//Give the server a map to respond the user resquest data from URLs.
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-//Listener
-app.listen(PORT, function(){
-    console.log('App listening on PORT: ' + PORT);
-});
+app.use(api);
+app.use(html);
 
 
-
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
